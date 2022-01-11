d3e7db72d73aca33f6911c6df82d8ffba169dcfd
Armando Miraglia <arma2ff0@gmail.com>
2022-01-11 13:46:43 +0100

    [Alacritty] Add alacritty config and setup
    This change adds alacritty configuration dir and file
    and the related setup steps.
    
    NOTE: to ease the configuration steps the setup.sh is now slightly
    refactored to check whether a non-symlink is present in the system
    and if so repalce it with a symlink to the dotfiles content.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
 main () {
-  ln -s ${REPO_PATH}/.gitconfig ${HOME}/.gitconfig
+  # Git Configurations.
+  if [[ ! -h ${HOME}/.gitconfig ]]
+  then
+    rm -r ${HOME}/.gitconfig
+    ln -s ${REPO_PATH}/.gitconfig ${HOME}/.gitconfig
+    # If the local file does not exist, it will be created.
+    # It will be poplulated separately with the more
+    # sensitive content.
+    touch ${HOME}/.gitconfig.local
+  fi
+
+  # Alacritty Configurations.
+  if [[ ! -h ${HOME}/.config/alacritty ]]
+  then
+    rm -r ${HOME}/.config/alacritty
+    ln -s ${REPO_PATH}/alacritty ${HOME}/.config/alacritty
+  fi
 }
 
 if [[ -n $BASH_SOURCE ]]
