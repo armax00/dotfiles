d86a573c863f71072abb7feac13bd88c5b98571b
Armando Miraglia <arma2ff0@gmail.com>
2022-01-11 20:38:21 +0100

    [WayBar] Some customization for the waybar
    This changes some of the parameters and icons to match my style.

--- setup.sh
+++ setup.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
-set -e
+set -xe
+
+install_config () {
+  local application_conf="$1"; shift
+  local repo_path="$1"; shift
+  local dst_path="$1"; shift
+
+  if [[ ! -h "$dst_path/$application_conf" ]]
+  then
+    echo "Creating $dst_path/$application_conf"
+    rm -fr "$dst_path/$application_conf"
+    ln -s "$repo_path/$application_conf" \
+        "$dst_path/$application_conf"
+  fi
+}
 
 main () {
+  local script_path=$(realpath $BASH_SOURCE)
+  local repo_path=$(dirname $script_path)
+
   # Git Configurations.
-  if [[ ! -h ${HOME}/.gitconfig ]]
-  then
-    rm -r ${HOME}/.gitconfig
-    ln -s ${REPO_PATH}/.gitconfig ${HOME}/.gitconfig
-    # If the local file does not exist, it will be created.
-    # It will be poplulated separately with the more
-    # sensitive content.
-    touch ${HOME}/.gitconfig.local
-  fi
+  install_config ".gitconfig" "${repo_path}" "${HOME}"
+  # If the local file does not exist, it will be created.
+  # It will be poplulated separately with the more
+  # sensitive content.
+  touch ${HOME}/.gitconfig.local
 
   # Alacritty Configurations.
-  if [[ ! -h ${HOME}/.config/alacritty ]]
-  then
-    rm -r ${HOME}/.config/alacritty
-    ln -s ${REPO_PATH}/alacritty ${HOME}/.config/alacritty
-  fi
+  install_config "alacritty" "${repo_path}" "${HOME}/.config"
+
+  # WayBar Configurations.
+  install_config "waybar" "${repo_path}" "${HOME}/.config"
 }
 
 if [[ -n $BASH_SOURCE ]]
 then
-  SCRIPT_PATH=$(realpath $BASH_SOURCE)
-  REPO_PATH=$(dirname $SCRIPT_PATH)
-
   main "$@"
 fi
